6e7ffc5805392cc3fe56e442155507acd138275b
FGargov <fgargov@gmail.com>
2024-08-19 22:07:43 +0300

    Add <include>**/JUnitTestRunner.java</include> tag in pom.xml file and fix "mvn test" problem.

--- src/test/java/CucumberOptions/ParallelTestRunner.java
+++ src/test/java/CucumberOptions/ParallelTestRunner.java
@@ -1,14 +1,14 @@
-//package CucumberOptions;
-//
-//import org.junit.runner.JUnitCore;
-//import org.junit.experimental.ParallelComputer;
-//
-//public class ParallelTestRunner {
-//    public static void main(String[] args) {
-//        // Използване на ParallelComputer за паралелно изпълнение на тестове
-//        Class<?>[] classes = {JUnitTestRunner.class}; // Заменете с вашия клас за JUnit рънър
-//
-//        // Изпълняване на тестовете паралелно
-//        JUnitCore.runClasses(new ParallelComputer(true, true), classes);
-//    }
-//}
+package CucumberOptions;
+
+import org.junit.runner.JUnitCore;
+import org.junit.experimental.ParallelComputer;
+
+public class ParallelTestRunner {
+    public static void main(String[] args) {
+        // Използване на ParallelComputer за паралелно изпълнение на тестове
+        Class<?>[] classes = {JUnitTestRunner.class}; // Заменете с вашия клас за JUnit рънър
+
+        // Изпълняване на тестовете паралелно
+        JUnitCore.runClasses(new ParallelComputer(true, true), classes);
+    }
+}
