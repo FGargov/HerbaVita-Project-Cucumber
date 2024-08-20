044ccf94ac33e08d515f167b992db47ddc50137e
FGargov <fgargov@gmail.com>
2024-08-20 03:12:02 +0300

    Add Gecko driver and headless option for him

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -2,6 +2,9 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 import java.io.FileInputStream;
@@ -20,10 +23,23 @@ public class TestBase  {
 
        if (driver == null) {
            if (prop.getProperty("browser").equalsIgnoreCase("chrome")) {
-               driver = new ChromeDriver();
+               ChromeOptions options = new ChromeOptions();
+
+               // Проверка дали headless режим трябва да бъде активиран
+               if (prop.getProperty("headless").equalsIgnoreCase("true")) {
+                   options.addArguments("headless");
+                   options.addArguments("window-size=1920,1080"); // Настройки за резолюция
+               }
+
+               driver = new ChromeDriver(options);
            }
            if (prop.getProperty("browser").equalsIgnoreCase("firefox")) {
-               //firefox code
+               FirefoxOptions options = new FirefoxOptions();
+
+               if (prop.getProperty("headless").equalsIgnoreCase("true")) {
+                   options.addArguments("--headless");
+               }
+               driver = new FirefoxDriver(options);
            }
            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
            driver.get(url);
