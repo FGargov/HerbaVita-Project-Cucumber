41d3c0d7e330e8727adf5566a5d4f8bee4648d53
FGargov <fgargov@gmail.com>
2024-09-11 22:52:24 +0300

    Add HomePage and HomePageStepDefinition classes

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,13 +1,14 @@
 package pageObjects;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class HomePage {
     private WebDriver driver;
@@ -20,7 +21,13 @@ public class HomePage {
 
     //private By homePageLogo = By.xpath("//a[@class='wd-logo wd-main-logo']//img");
     private By homePageLogo = By.xpath("//a[@class='wd-logo wd-main-logo']//img");
-    private By menuItems = By.xpath("//span[contains(@class,'menu-opener color-scheme-light')]");
+    private By menuItems = By.xpath("//ul[@id='menu-categories']//li/a");
+    private By offerFrame = By.xpath("//rs-slide[@data-anim='ms:600;']");
+    private By buyNowButton = By.xpath("//*[@id='slider-45-slide-110-layer-10']");
+
+    public WebDriverWait getWait() {
+        return this.wait;
+    }
 
     public String getTitleHomePage() {
         return driver.getTitle();
@@ -35,4 +42,32 @@ public class HomePage {
     public boolean isLogoDisplayed() {
        return driver.findElement(homePageLogo).isDisplayed();
     }
+
+    public List<WebElement> getNavigationMenuItems() {
+        return driver.findElements(menuItems);
+    }
+
+    public void clickMenuItem(WebElement menuItem) {
+        menuItem.click();
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public void clickOnPromoButton(WebElement button) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", button);
+    }
+
+    public WebElement getBuyNowButton() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(buyNowButton));
+        return driver.findElement(buyNowButton);
+    }
+
+    public boolean getOfferFrame() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(offerFrame));
+        return driver.findElement(offerFrame).isDisplayed();
+    }
+
 }
--- src/test/java/stepDefinitions/HomePageStepDefinition.java
+++ src/test/java/stepDefinitions/HomePageStepDefinition.java
@@ -2,17 +2,23 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import pageObjects.HomePage;
+import pageObjects.OfferPage;
 import utils.TestContextSetup;
 
 public class HomePageStepDefinition {
     private TestContextSetup testContextSetup;
     private HomePage homePage;
+    private OfferPage offerPage;
 
     public HomePageStepDefinition(TestContextSetup testContextSetup) {
         this.testContextSetup = testContextSetup;
         homePage = testContextSetup.getPageObjectManager().getHomePage();
+        offerPage = testContextSetup.getPageObjectManager().getOfferPage();
     }
 
     @Given("I am on the home page")
@@ -21,13 +27,64 @@ public class HomePageStepDefinition {
     }
 
     @Then("I should be on the correct home page URL")
-    public void iShouldBeOnCorrectHomePageURL() {
+    public void shouldBeOnCorrectHomePageURL() {
         Assert.assertTrue("The home page URL is incorrect", homePage.isHomePageURLCorrect());
     }
 
 
     @Then("I should see the site logo displayed correctly")
-    public void iShouldSeeSideLogoIsDisplayed() {
+    public void shouldSeeSideLogoIsDisplayed() {
         Assert.assertTrue("The site logo is not displayed correctly", homePage.isLogoDisplayed());
     }
+
+    @When("I click on first menu item and I should be directed to the correct page")
+    public void clickOnFirstNavigationMenuItem() {
+            WebElement firstMenuItem = homePage.getNavigationMenuItems().get(0);
+
+            String expectedUrl = firstMenuItem.getAttribute("href");
+            homePage.clickMenuItem(firstMenuItem);
+            homePage.getWait().until(ExpectedConditions.urlToBe(expectedUrl));
+
+            String currentUrl = homePage.getCurrentUrl();
+            Assert.assertEquals("The redirection is not to the correct page!", expectedUrl, currentUrl);
+    }
+
+    @When("I click on second menu item and I should be directed to the correct page")
+    public void clickOnSecondNavigationMenuItem() {
+        WebElement firstMenuItem = homePage.getNavigationMenuItems().get(1);
+
+        String expectedUrl = firstMenuItem.getAttribute("href");
+        homePage.clickMenuItem(firstMenuItem);
+        homePage.getWait().until(ExpectedConditions.urlToBe(expectedUrl));
+
+        String currentUrl = homePage.getCurrentUrl();
+        Assert.assertEquals("The redirection is not to the correct page!", expectedUrl, currentUrl);
+    }
+
+    @When("I should see promotional banners displayed on the home page")
+    public void shouldSeePromotionalBanners() {
+        Assert.assertTrue("The promotional banner is not displayed correctly", homePage.getOfferFrame());
+    }
+
+    @Then("I click on the promotional banner and I should be directed to the promotional page")
+    public void clickOnOfferBannerAndDirectedToPromoPage() {
+
+        homePage.clickOnPromoButton(homePage.getBuyNowButton());
+
+        verifyOfferPageUrl();
+        verifyProductTitle();
+    }
+
+    private void verifyOfferPageUrl() {
+        String currentUrl = homePage.getCurrentUrl();
+        String expectedUrl = offerPage.getCurrentUrl();
+
+        Assert.assertEquals("The redirection is not to the correct page!", expectedUrl, currentUrl);
+    }
+
+    private void verifyProductTitle() {
+        String productTitle = offerPage.getProductTitle();
+        Assert.assertEquals("The product title is not displayed correctly", "CR7 DRIVE С ВКУС НА АКАЙ БЕРИ", productTitle);
+    }
+
 }
