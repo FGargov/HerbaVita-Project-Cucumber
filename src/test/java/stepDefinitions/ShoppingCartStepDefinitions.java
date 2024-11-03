b4d258cebb41af1f1f351b7b5e6a0da93d95d6db
FGargov <fgargov@gmail.com>
2024-11-03 12:32:24 +0200

    Refactor shopping cart step definitions to use TestData class

--- src/test/java/stepDefinitions/ShoppingCartStepDefinitions.java
+++ src/test/java/stepDefinitions/ShoppingCartStepDefinitions.java
@@ -3,9 +3,9 @@ package stepDefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
 import pageObjects.ShoppingCartPage;
 import utils.TestContextSetup;
+import utils.TestData;
 
 public class ShoppingCartStepDefinitions {
     private TestContextSetup testContextSetup;
@@ -17,7 +17,8 @@ public class ShoppingCartStepDefinitions {
     }
 
     @When("^I search for (.*) in the search field and click on the search button$")
-    public void searchForProductAndClickOnSearchButton(String productName) {
+    public void searchForProductAndClickOnSearchButton(String productKey) {
+        String productName = TestData.getProductNameFromShoppingCart(productKey);
         shoppingCartPage.searchForProduct(productName);
         shoppingCartPage.clickOnSearchButton(shoppingCartPage.getSearchButton());
 
@@ -25,24 +26,30 @@ public class ShoppingCartStepDefinitions {
     }
 
     @Then("^I have added (.*) to my shopping cart$")
-    public void addAllProductsToShoppingCart(String productName) {
+    public void addAllProductsToShoppingCart(String productKey) {
+        String productName = TestData.getProductNameFromShoppingCart(productKey);
         shoppingCartPage.addProductToCart(productName);
         shoppingCartPage.verifyShoppingCartIsNotEmpty();
     }
 
-    @And("^I change the quantity of (.*) to (.*)$")
-    public void changeQuantityOfProduct(String productName, String quantity) {
-        shoppingCartPage.changeProductQuantity(productName, quantity);
+    @And("^I change the quantity of (.*) to (\\d+)$")
+    public void changeQuantityOfProduct(String productKey) {
+        String productName = TestData.getProductNameFromShoppingCart(productKey);
+        int quantityFromJson = TestData.getProductQuantityFromShoppingCart(productKey);
+        shoppingCartPage.changeProductQuantity(productName, String.valueOf(quantityFromJson));
     }
 
     @And("^I should see the updated quantity of (.*) in my shopping cart and it should be (.*)$")
-    public void shouldSeeTheUpdatedQuantity(String productName, int expectedQuantity) {
+    public void shouldSeeTheUpdatedQuantity(String productKey) {
+        String productName = TestData.getProductNameFromShoppingCart(productKey);
+        int quantityFromJson = TestData.getProductQuantityFromShoppingCart(productKey);
         int actualQuantity = shoppingCartPage.getProductQuantity(productName);
-        shoppingCartPage.verifyUpdatedQuantity(expectedQuantity, actualQuantity);
+        shoppingCartPage.verifyUpdatedQuantity(quantityFromJson, actualQuantity);
     }
 
     @And("^I remove (.*) from my shopping cart$")
-    public void removeProductFromCart(String productName) {
+    public void removeProductFromCart(String productKey) {
+        String productName = TestData.getProductNameFromShoppingCart(productKey);
         shoppingCartPage.removeProductFromCart(productName);
         shoppingCartPage.verifyListOfProductsIsEmpty();
     }
